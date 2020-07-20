7837df67f06ed7b621cba75d925623511b691c60
Brandon Peterson <hoogguns@users.noreply.github.com>
2020-07-20 16:54:26 -0600

    Update moment.js
    Sorry about the other 2 pull requests, I am new to GitHub (as far as contributing to projects goes) I am taking an expanded coding class and am learning git as part of the curriculum, until now I have used git for cloning; looks like Fred has a solution, I think this is also a viable solution.

--- src/utils/moment.js
+++ src/utils/moment.js
@@ -1,6 +1,7 @@
 import moment from "moment"
+import "moment/min/locales"
 
 export const getLocaleTimestamp = (locale, timestamp) => {
   moment.locale(locale)
-  return moment(timestamp).format("MMM DD, YYYY")
+  return moment(timestamp).format("MMM Do, YYYY")
 }
